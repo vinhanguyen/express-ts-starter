368e7c9414d9c48c1387729389ddb75aa2d668fb
vinhanguyen <vinh.a.nguyen@gmail.com>
2021-05-06 23:31:10 -0400

    Set src and output directories.  Install express type declarations.  Create express app.

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('it works!');
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
